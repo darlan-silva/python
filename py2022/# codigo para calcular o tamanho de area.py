28f9f11ebbcd503d621a44f6499487bee13fbf01
darlan r da silva <darlan.silva887@live.com>
2023-06-15 11:04:31 -0300

    Create # codigo para calcular o tamanho de area.py

---
+++ py2022/# codigo para calcular o tamanho de area.py
@@ -0,0 +1,6 @@
+# codigo para calcular o tamanho de area de um circulo dando entrada dos dados
+pi = 3.14
+print("Ola , tudo bem ?\nvamos calcular a area de  circulo em cm ")
+raio =int(input("digite o raio do circulo "))
+print(" certo, estamos calculando")
+print("o a area do circulo é "+str(pi*raio**2)," em cm2")
